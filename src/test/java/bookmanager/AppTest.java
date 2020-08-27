f18ecb42d7a12cdb6ffdd80ce251681ded72bb67
ned <ned@me.com>
2020-08-27 22:15:41 +0200

    added dummy junit tests just to check if CI works

---
+++ src/test/java/bookmanager/AppTest.java
@@ -0,0 +1,29 @@
+package bookmanager;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+    private App app;
+
+    @Before
+    public void setup() {
+        app = new App();
+    }
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hello", app.sayHello());
+    }
+
+    @Test
+    public void testSayHelloWithName() {
+        assertEquals("Hello World", app.sayHello("World"));
+    }
+
+    @Test
+    public void testSayHelloWithAnotherName() {
+        assertEquals("Hello Another World", app.sayHello("Another World"));
+    }
+}
