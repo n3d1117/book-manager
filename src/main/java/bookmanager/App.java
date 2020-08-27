5d8d955adf24b2150076f792da302b80fe888fcf
ned <n3d1117@users.noreply.github.com>
2020-08-27 18:19:04 +0200

    testing pull request (#1)
    * added dummy data
    * Explicit dep jaxb-api to avoid problems with JDK9
    * added GITHUB_TOKEN to ci

---
+++ src/main/java/bookmanager/App.java
@@ -0,0 +1,14 @@
+package bookmanager;
+
+public class App {
+    public String sayHello(String name) {
+        if (name == null)
+            return "Hello";
+        else
+            return "Hello " + name;
+    }
+
+    public String sayHello() {
+        return sayHello(null);
+    }
+}
