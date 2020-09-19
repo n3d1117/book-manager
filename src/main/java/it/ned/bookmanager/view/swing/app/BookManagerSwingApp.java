c5514fb78eca18590a1dbf348ba4b12766ca04a0
ned <ned@me.com>
2020-09-19 11:39:10 +0200

    added picocli dependency and app arguments

--- src/main/java/it/ned/bookmanager/view/swing/app/BookManagerSwingApp.java
+++ src/main/java/it/ned/bookmanager/view/swing/app/BookManagerSwingApp.java
@@ -2,6 +2,7 @@ package it.ned.bookmanager.view.swing.app;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+
 import it.ned.bookmanager.controller.BookManagerController;
 import it.ned.bookmanager.service.AuthorService;
 import it.ned.bookmanager.service.BookService;
@@ -10,38 +11,65 @@ import it.ned.bookmanager.service.transactional.BookTransactionalService;
 import it.ned.bookmanager.transaction.TransactionManager;
 import it.ned.bookmanager.transaction.mongo.TransactionMongoManager;
 import it.ned.bookmanager.view.swing.BookManagerSwingView;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 import java.awt.*;
+import java.util.concurrent.Callable;
 
-public class BookManagerSwingApp {
-
-    private static final String DB_NAME = "bookmanager";
-    private static final String DB_AUTHOR_COLLECTION = "authors";
-    private static final String DB_BOOK_COLLECTION = "books";
+@Command(mixinStandardHelpOptions=true)
+public class BookManagerSwingApp implements Callable<Void> {
 
     private static final Logger LOGGER = LogManager.getLogger(BookManagerSwingApp.class);
 
+    @Option(names = { "--mongo-url" }, description = "MongoDB replica set URL")
+    private static final String MONGO_URL = "mongodb://localhost:27017";
+
+    @Option(names = { "--db-name" }, description = "Database name")
+    private static final String DB_NAME = "bookmanager";
+
+    @Option(names = { "--db-author-collection" }, description = "Authors collection name")
+    private static final String AUTHOR_COLLECTION = "authors";
+
+    @Option(names = { "--db-book-collection" }, description = "Books collection name")
+    private static final String BOOK_COLLECTION = "books";
+
     public static void main(String[] args) {
         LOGGER.info("App started");
+        new CommandLine(new BookManagerSwingApp()).execute(args);
+    }
 
+    @Override
+    public Void call() {
         EventQueue.invokeLater(() -> {
-            BookManagerSwingView view = new BookManagerSwingView();
+            try {
+                BookManagerSwingView view = new BookManagerSwingView();
 
-            MongoClient client = MongoClients.create("mongodb://localhost:27017");
-            TransactionManager transactionManager = new TransactionMongoManager(client, DB_NAME,
-                    DB_AUTHOR_COLLECTION, DB_BOOK_COLLECTION);
+                MongoClient client = MongoClients.create(MONGO_URL);
+                TransactionManager transactionManager = new TransactionMongoManager(client, DB_NAME,
+                        AUTHOR_COLLECTION, BOOK_COLLECTION);
 
-            AuthorService authorService = new AuthorTransactionalService(transactionManager);
-            BookService bookService = new BookTransactionalService(transactionManager);
+                AuthorService authorService = new AuthorTransactionalService(transactionManager);
+                BookService bookService = new BookTransactionalService(transactionManager);
 
-            BookManagerController controller = new BookManagerController(authorService, bookService, view);
-            view.setController(controller);
+                BookManagerController controller = new BookManagerController(authorService, bookService, view);
+                view.setController(controller);
 
-            view.pack();
-            view.setLocationRelativeTo(null); // this centers the view
-            view.setVisible(true);
+                view.pack();
+                view.setLocationRelativeTo(null); // this centers the view
+                view.setVisible(true);
+
+                controller.allAuthors();
+                controller.allBooks();
+            } catch (Exception e) {
+                LOGGER.debug(() -> String.format("Caught Exception: %s", e.getMessage()));
+            }
         });
+        return null;
     }
 }
